c67838fa085d7afe8dd83f75e32f058fd8c01532
Gabriel <gdeoliveira.1999@gmail.com>
2021-02-05 17:11:42 -0400

    Absolute Imports configured

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
-import Layout from '../components/Layout'
+import Layout from 'src/components/Layout';
 
 export default function Home() {
   return (
     <Layout title="Youtube MUI">
       Clone YouTube w/ Next.js + Material-UI
-      <Button variant="outlined" color="secondary">Hello World</Button>
+      <Button variant="outlined" color="secondary">
+        Hello World
+      </Button>
     </Layout>
   );
 }
