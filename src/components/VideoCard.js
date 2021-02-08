c449eaf3dc07cd76787ebabe4c77db9714aeb511
Gabriel <gdeoliveira.1999@gmail.com>
2021-02-08 02:13:49 -0400

    Homepage finished

---
+++ src/components/VideoCard.js
@@ -0,0 +1,59 @@
+import { Box, Typography, Avatar, makeStyles } from '@material-ui/core';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(relativeTime);
+
+const useStyles = makeStyles(() => ({
+  img: {
+    width: '100%',
+  },
+  avatar: {
+    marginRight: '8px',
+  },
+  caption: {
+    fontWeight: 500,
+    display: '-webkit-box',
+    '-webkit-line-clamp': 2,
+    '-webkit-box-orient': 'vertical',
+    overflow: 'hidden',
+  },
+}));
+
+function VideoCard({ item }) {
+  const classes = useStyles();
+  return (
+    <Box>
+      <img src={item.thumb} alt={item.title} className={classes.img} />
+      <Box display="flex" mt="1">
+        <Box>
+          <Avatar
+            alt={item.authorName}
+            src={item.authorAvatar}
+            className={classes.avatar}
+          >
+            SS
+          </Avatar>
+        </Box>
+        <Box>
+          <Typography
+            className={classes.caption}
+            gutterBottom
+            variant="body1"
+            color="textPrimary"
+          >
+            {item.title}
+          </Typography>
+          <Typography display="block" variant="body2" color="textSecondary">
+            {item.authorName}
+          </Typography>
+          <Typography variant="body2" color="textSecondary">
+            {`${item.views} views • ${dayjs(item.updatedAt).fromNow()}`}
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+export default VideoCard;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,80 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { Box, Grid } from '@material-ui/core';
 import Layout from 'src/components/Layout';
+import VideoCard from 'src/components/VideoCard';
 
-export default function Home() {
+function Home({ data }) {
   return (
     <Layout title="Youtube MUI">
-      Clone YouTube w/ Next.js + Material-UI
-      <Button variant="outlined" color="secondary">
-        Hello World
-      </Button>
+      <Box p={2}>
+        <Grid container spacing={4}>
+          {data.map((item) => (
+            <Grid key={item.id} item xl={3} lg={3} md={4} sm={6} xs={12}>
+              <VideoCard item={item} />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </Layout>
   );
 }
+
+export async function getStaticProps() {
+  const data = [
+    {
+      id: 1,
+      title:
+        'Midnight Coding in Chicago | LoFi Jazz Hip-Hop [Code - Relax - Study]',
+      authorId: 1,
+      authorName: 'Code Pioneers',
+      authorAvatar: 'avatarUrl',
+      views: 450,
+      thumb: '/thumbs/next01.webp',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+    {
+      id: 2,
+      title:
+        'COMO MELHORAR SEU CODIGO JAVASCRIPT (ESLINT + PRETTIER + EDITORCONFIG) | Dicas e Truques #02',
+      authorId: 2,
+      authorName: 'Lucas Nhimi',
+      authorAvatar: 'avatarUrl',
+      views: 824310,
+      thumb: '/thumbs/next02.webp',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+    {
+      id: 3,
+      title:
+        'RESPONSIVE Personal PORTFOLIO Website HTML CSS And JAVASCRIPT | Mobile First',
+      authorId: 3,
+      authorName: 'Bedimcode',
+      authorAvatar: 'avatarUrl',
+      views: 20112,
+      thumb: '/thumbs/next03.webp',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+    {
+      id: 4,
+      title:
+        'Como a STARLINK vai mudar o FUTURO da Internet | A Internet do Futuro',
+      authorId: 4,
+      authorName: 'Elementar',
+      authorAvatar: 'avatarUrl',
+      views: 10937,
+      thumb: '/thumbs/next04.webp',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+  ];
+  return {
+    props: {
+      data: JSON.parse(JSON.stringify(data)),
+    },
+  };
+}
+
+export default Home;
